acb12913d9d71e71b771cd89f67838257f1693ec
Eldar Cajic <el.cajic@gmail.com>
2020-02-18 11:14:32 +0100

    removed backend

---
+++ build/precache-manifest.c7e654c34c11370f1f74eb284ff6e64c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d66edd1cef89d5422247dfeb5ddf5a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36bcc4b8540682b9404a",
+    "url": "/static/css/2.a90e374a.chunk.css"
+  },
+  {
+    "revision": "974f8f915987a7a6fd00",
+    "url": "/static/css/main.64b0c3c4.chunk.css"
+  },
+  {
+    "revision": "36bcc4b8540682b9404a",
+    "url": "/static/js/2.4da76ba8.chunk.js"
+  },
+  {
+    "revision": "e15d647bc072a6f875e2edaa1d977880",
+    "url": "/static/js/2.4da76ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974f8f915987a7a6fd00",
+    "url": "/static/js/main.597adf23.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
